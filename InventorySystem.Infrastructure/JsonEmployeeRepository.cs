ccac621dbe055069133a4ae5faedbd7da231be29
gabriel-biagi <gabrielfelipebiagi@gmail.com>
2026-07-27 18:09:54 -0400

    feat(infrastructure): implement JsonEmployeeRepository methods using JSON storage

--- InventorySystem.Infrastructure/JsonEmployeeRepository.cs
+++ InventorySystem.Infrastructure/JsonEmployeeRepository.cs
@@ -4,13 +4,45 @@ namespace InventorySystem.Infrastructure
 {
     public class JsonEmployeeRepository : IEmployeeRepository
     {
+        private string _filePath = "Data/employees.json";
         public Employee? GetByRegistration(int registration)
         {
-            throw new NotImplementedException();
+            var lista = LoadEmployeesFromFile();
+            return lista.FirstOrDefault(x => registration == x.Registration);
         }
         public void Add(Employee employee)
         {
-            throw new NotImplementedException();
+            var lista = LoadEmployeesFromFile();
+            lista.Add(employee);
+            SaveEmployeesToFile(lista);
+        }
+
+        public void SaveEmployeesToFile(List<Employee> employees)
+        {
+            _ensureDirectory();
+            string json = System.Text.Json.JsonSerializer.Serialize(employees);
+            File.WriteAllText(_filePath, json);
+        }
+
+        public List<Employee> LoadEmployeesFromFile()
+        {
+            _ensureDirectory();
+            string json = File.ReadAllText(_filePath);
+            return System.Text.Json.JsonSerializer.Deserialize<List<Employee>>(json) ?? new List<Employee>();
+        }
+
+        private void _ensureDirectory()
+        {
+            string directory = Path.GetDirectoryName(_filePath)!;
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            if (!File.Exists(_filePath))
+            {
+                File.WriteAllText(_filePath, "[]");
+            }
         }
     }
 }
