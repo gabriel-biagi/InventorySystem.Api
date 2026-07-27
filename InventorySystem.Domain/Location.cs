5e7c726bac609907c7614e7c638fde8daad873c9
gabriel-biagi <gabrielfelipebiagi@gmail.com>
2026-07-27 16:41:45 -0400

    feat(domain): add Location value object

---
+++ InventorySystem.Domain/Location.cs
@@ -0,0 +1,16 @@
+﻿namespace InventorySystem.Domain
+{
+    public class Location
+    {
+        public int Column { get; private set; }
+        public int Shelf { get; private set; }
+        public int Item { get; private set; }
+
+        public Location(int column, int shelf, int item)
+        {
+            Column = column;
+            Shelf = shelf;
+            Item = item;
+        }
+    }
+}
