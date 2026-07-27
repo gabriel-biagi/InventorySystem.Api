1be75ea035bc3df6e3ee9cf63479e3753de38b36
gabriel-biagi <gabrielfelipebiagi@gmail.com>
2026-07-27 16:35:13 -0400

    feat(domain): add IEmployeeRepository interface

---
+++ InventorySystem.Domain/IEmployeeRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace InventorySystem.Domain
+{
+    public interface IEmployeeRepository
+    {
+        Employee? GetByRegistration(int registration);
+        void Add(Employee employee);
+    }
+}
