bc36941ac1d78a92c80470cca389ebcbfcbfe389
gabriel-biagi <gabrielfelipebiagi@gmail.com>
2026-07-27 16:22:00 -0400

    feat(infrastructure): add JsonProductRepository skeleton implementing IProductRepository

---
+++ InventorySystem.Infrastructure/JsonProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using InventorySystem.Domain;
+
+namespace InventorySystem.Infrastructure;
+
+public class JsonProductRepository : IProductRepository
+{
+    public Product? GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Add(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
