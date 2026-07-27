f8d957b746cbf37130a4a63cd3cd3ed8c8fc6bb9
gabriel-biagi <gabrielfelipebiagi@gmail.com>
2026-07-27 16:15:04 -0400

    feat(domain): add Product entity and UnitType enum

---
+++ InventorySystem.Domain/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem.Domain
+{
+    public class Product
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public UnitType UnitType { get; private set; }
+
+        public Product(string name, int id, UnitType unitType)
+        {
+            Name = name;
+            Id = id;
+            UnitType = unitType;
+        }
+
+        public void UpdateName(string name)
+        {
+            Name = name;
+        }
+    }
+}
