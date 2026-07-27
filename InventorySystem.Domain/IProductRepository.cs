347c344681ebc82b805e65e0c9e02bf6cc4b8e75
gabriel-biagi <gabrielfelipebiagi@gmail.com>
2026-07-27 16:18:32 -0400

    feat(domain): add IProductRepository interface

---
+++ InventorySystem.Domain/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace InventorySystem.Domain
+{
+    public interface IProductRepository
+    {
+        Product? GetById(int id);
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(int id);
+    }
+}
