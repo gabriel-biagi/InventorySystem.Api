5f8a08dbd7c330c63b5fe01ac4754d488ffb737f
gabriel-biagi <gabrielfelipebiagi@gmail.com>
2026-08-03 21:48:40 -0400

    feat(api): implement products controller with get products action
    - Add AppDbContext injection via constructor
    - Implement GetProducts action with empty list check

---
+++ InventorySystem.Api/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using InventorySystem.Domain.Entities;
+using InventorySystem.Infrastructure.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventorySystem.Api;
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private  readonly AppDbContext _context;
+        
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            var products = _context.Products.ToList();
+            if (!products.Any())
+            {
+                return NotFound("No products found");
+            }
+            return products;
+        }
+    }
